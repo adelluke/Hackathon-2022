f81ba37cac17e614ad61956ef15bf24c8fdf6bde
DrYogurt <ben.z.rubinstein@gmail.com>
2022-11-16 11:51:09 -0500

    Adding Code we wrote last night 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,123 @@
-/* Pro Micro Test Code
-   by: Nathan Seidle
-   modified by: Jim Lindblom
-   SparkFun Electronics
-   date: September 16, 2013
-   license: Public Domain - please use this code however you'd like.
-   It's provided as a learning tool.
-
-   This code is provided to show how to control the SparkFun
-   ProMicro's TX and RX LEDs within a sketch. It also serves
-   to explain the difference between Serial.print() and
-   Serial1.print().
-*/
+#include <Wire.h>
+#include <SFE_BMP180.h>
+#include <SPIMemory.h>
 #include <Arduino.h>
+#define Serial SERIAL_PORT_USBVIRTUAL
 
-int RXLED = 17;  // The RX LED has a defined Arduino pin
-// Note: The TX LED was not so lucky, we'll need to use pre-defined
-// macros (TXLED1, TXLED0) to control that.
-// (We could use the same macros for the RX LED too -- RXLED1,
-//  and RXLED0.)
+#define BAUD_RATE 9600
+#define CS_PIN 7
 
-void setup()
-{
-  pinMode(RXLED, OUTPUT);  // Set RX LED as an output
-  // TX LED is set as an output behind the scenes
 
-  Serial.begin(9600); //This pipes to the serial monitor
-  Serial.println("Initialize Serial Monitor");
+SPIFlash flash(CS_PIN);
+//bool readSerialStr(String &inputStr);
 
-  Serial1.begin(9600); //This is the UART, pipes to sensors attached to board
-  Serial1.println("Initialize Serial Hardware UART Pins");
+
+SFE_BMP180 bmp180;
+double baseline;
+int current_address;
+int RXLED = 17; 
+
+// Byte Arrays
+byte * BtAr(double num1, double num2, long num3) {
+  byte arr[24];
+  for (int i = 0; i < 3; i++) {
+    Serial.println(i);
+    switch (i) {
+      case 0:
+        for (int j = 0; j < 8; j++)
+          arr[j] = ((byte *)&num1)[j];
+        Serial.println("phew");
+        break;
+      case 1:
+        for (int j = 0; j < 8; j++)
+          arr[j+8] = ((byte *)&num2)[j];
+        break;
+      case 2:
+        for (int j = 0; j < 8; j++)
+          arr[j+16] = ((byte *)&num3)[j];
+        break;
+      default:
+        Serial.println("Ya done fuqed up");
+    }
+  }
 }
 
-void loop()
-{
-  Serial.println("Hello world!");  // Print "Hello World" to the Serial Monitor
-  Serial1.println("Hello! Can anybody hear me?");  // Print "Hello!" over hardware UART
+double getPressure() {
+  char status;
+  double T,P,p0,a;
+  Serial.println("in getPressure");
 
-  digitalWrite(RXLED, LOW);   // set the RX LED ON
-  TXLED0; //TX LED is not tied to a normally controlled pin so a macro is needed, turn LED OFF
-  delay(1000);              // wait for a second
+  status = bmp180.startTemperature();
+  if (status != 0) {
+    delay(status);
+    status = bmp180.getTemperature(T);
+    if (status != 0) {
+      status = bmp180.startPressure(3);
+      if (status != 0) {
+        delay(status);
+        status = bmp180.getPressure(P,T);
+        if (status != 0) {
+          Serial.println("got pressure!");
+          return(P);
+        }
+        else Serial.println("error retrieving pressure measurement\n");
+        digitalWrite(RXLED, LOW);
+      }
+      else Serial.println("error starting pressure measurement\n");
+      digitalWrite(RXLED, LOW);
+    }
+    else Serial.println("error retrieving temperature measurement\n");
+    digitalWrite(RXLED, LOW);
+  }
+  else Serial.println("error starting temperature measurement\n");
+  digitalWrite(RXLED, LOW);
+}
 
-  digitalWrite(RXLED, HIGH);    // set the RX LED OFF
-  TXLED1; //TX LED macro to turn LED ON
-  delay(1000);              // wait for a second
+
+// Real Code Time
+void setup() {
+  Serial.begin(BAUD_RATE);
+  pinMode(RXLED, OUTPUT);
+  
+  while(!Serial);
+
+  if (bmp180.begin()) {
+    Serial.println("BMP180 init success");
+    digitalWrite(RXLED, HIGH);
+
+  } else {
+    //while (1);
+    Serial.println("bmp unsuccessful");
+  }
+    delay(200);
+  baseline = getPressure();
+}
+
+void loop() {
+  Serial.println("in the loop!");
+  delay(500);
+  double a,P;
+  long t;
+  P = getPressure();
+  Serial.println("got P");
+  a = bmp180.altitude(P,baseline);
+  Serial.println("got a");
+  t = millis();
+  Serial.println("got t");
+  // call function to get array of bytes
+  byte * arrStore = BtAr(a, P, t);
+  Serial.println("russell's BS worked maybe");
+  // find the next empty spot
+  
+  if (flash.writeByteArray(current_address, arrStore, 24)) {
+    current_address += 24;
+    digitalWrite(RXLED, LOW);
+  } else {
+    Serial.println("write failed :(");
+  }
+
+  if(Serial){
+    Serial.println("thank you for plugging me in daddy");
+  }
+
 }
